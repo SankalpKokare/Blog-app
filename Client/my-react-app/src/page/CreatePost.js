6ac55f9517d6024b145e1f189215eefff34d48c6
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-29 23:32:06 +0530

    Added frontend for create post

---
+++ Client/my-react-app/src/page/CreatePost.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
+
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link', 'image'],
+        ['clean']
+    ],
+};
+
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+];
+
+function CreatePost() {
+
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+    const [files, setFiles] = useState('');
+
+
+    function createNewPost(e) {
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        //   data.set('file',)
+        e.preventDefault();
+        console.log(files);
+        fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data,
+        })
+
+    }
+
+    return (
+
+        <form onSubmit={createNewPost} >
+            <input type="title" placeholder={'Title'} value={title} onChange={e => { setTitle(e.target.value) }} />
+            <input type="summary" placeholder='Summary' value={summary} onChange={e => { setSummary(e.target.value) }} />
+            <input type="file" onChange={e => setFiles(e.target.files)} />
+            <ReactQuill value={{ content }} modules={modules} formats={formats} />
+            <button style={{ marginTop: '5px' }} value={content} onChange={newValue => setContent(newValue)}>Create post</button>
+        </form>
+    )
+}
+
+export default CreatePost
