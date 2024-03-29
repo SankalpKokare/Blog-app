8290eeab6677a4b8c045726663ae7393fa2c4a98
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-29 17:58:59 +0530

    completed registration, added  bcrypt

--- Api/index.js
+++ Api/index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -12,10 +14,11 @@ mongoose.connect('mongodb+srv://blog:9Nu1nB03zo69rgof@cluster0.sg2s1ub.mongodb.n
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
-        const userDoc = await User.create({ username, password });
+        const userDoc = await User.create({ username, password: bcrypt.hashSync(password, salt) });
         res.json(userDoc)
     } catch (e) {
-            res.status(400).json(e);
+        console.log(e);
+        res.status(400).json(e);
     }
 
 })
