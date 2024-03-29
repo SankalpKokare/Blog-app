e73873ad0d79c19dcaf71960f549b6e3ef5d0270
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-29 14:53:30 +0530

    added router, login, regiter page and structured pages

--- Client/my-react-app/src/App.js
+++ Client/my-react-app/src/App.js
@@ -1,70 +1,24 @@
+import React from 'react';
 import './App.css';
-
+import Layout from './Layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Post from './Post';
+import IndexPage from './page/IndexPage';
+import LoginPage from './page/LoginPage';
+import RegisterPage from './page/RegisterPage';
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2024/03/Amazon-One-app.jpg?w=1390&crop=1" alt="" />
-        </div>
-
-        <div className="texts">
-
-          <h2>Startup launches new app to scan your palm for checkout</h2>
-          <p className="info">
-            <a href="" className="author">Sankalp Kokare</a>
-            <time>2024-03-28  20:40</time>
-          </p>
-          <p className="summary"> the launch of its new app for Amazon One, its contactless palm recognition service that allows customers to hover their palm over a device in order to purchase from select places, including over 500 Whole Foods Market stores, Amazon stores, and more than 150 third-party locations.</p>
-
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2024/03/Amazon-One-app.jpg?w=1390&crop=1" alt="" />
-        </div>
-
-        <div className="texts">
-
-          <h2>Startup launches new app to scan your palm for checkout</h2>
-          <p className="info">
-            <a href="" className="author">Sankalp Kokare</a>
-            <time>2024-03-28  20:40</time>
-          </p>
-          <p className="summary"> the launch of its new app for Amazon One, its contactless palm recognition service that allows customers to hover their palm over a device in order to purchase from select places, including over 500 Whole Foods Market stores, Amazon stores, and more than 150 third-party locations.</p>
-
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2024/03/Amazon-One-app.jpg?w=1390&crop=1" alt="" />
-        </div>
-
-        <div className="texts">
-
-          <h2>Startup launches new app to scan your palm for checkout</h2>
-          <p className="info">
-            <a href="" className="author">Sankalp Kokare</a>
-            <time>2024-03-28  20:40</time>
-          </p>
-          <p className="summary"> the launch of its new app for Amazon One, its contactless palm recognition service that allows customers to hover their palm over a device in order to purchase from select places, including over 500 Whole Foods Market stores, Amazon stores, and more than 150 third-party locations.</p>
-
-        </div>
-      </div>
-
-   
-
-    </main>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />} >
+            <Route index element={<IndexPage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/Register' element={<RegisterPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ Client/my-react-app/src/Post.js
@@ -0,0 +1,20 @@
+export default function Post(){
+        return (
+            <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/03/Amazon-One-app.jpg?w=1390&crop=1" alt="" />
+        </div>
+
+        <div className="texts">
+
+          <h2>Startup launches new app to scan your palm for checkout</h2>
+          <p className="info">
+            <a href="" className="author">Sankalp Kokare</a>
+            <time>2024-03-28  20:40</time>
+          </p>
+          <p className="summary"> the launch of its new app for Amazon One, its contactless palm recognition service that allows customers to hover their palm over a device in order to purchase from select places, including over 500 Whole Foods Market stores, Amazon stores, and more than 150 third-party locations.</p>
+
+        </div>
+      </div>
+        )
+};
