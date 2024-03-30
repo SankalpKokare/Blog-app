2757da59f76187368c98f7ae382cffe1f2549c77
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-30 21:44:33 +0530

    Put all the multer editor into Editor Component

---
+++ Client/my-react-app/src/page/EditPost.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import { Navigate } from "react-router-dom";
+import Editor from "../Editor";
+
+
+function EditPost() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  function updatePost(e) {
+    e.preventDefault();
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <form onSubmit={updatePost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <input
+        type="summary"
+        placeholder="Summary"
+        value={summary}
+        onChange={(e) => {
+          setSummary(e.target.value);
+        }}
+      />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
+      <Editor onChange={setContent} value={content} />
+      <button style={{ marginTop: "5px" }}>Create post</button>
+    </form>
+  );
+}
+
+export default EditPost;
