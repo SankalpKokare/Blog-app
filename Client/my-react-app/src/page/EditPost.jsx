e2859122b567983b1be8b5c917dbe9911fa33ea9
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-30 22:24:01 +0530

    added edit post on react and api endpoint

--- Client/my-react-app/src/page/EditPost.jsx
+++ Client/my-react-app/src/page/EditPost.jsx
@@ -1,26 +1,54 @@
-import React, { useState } from "react";
-import ReactQuill from "react-quill";
-import { Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
 import Editor from "../Editor";
 
-
 function EditPost() {
+  const { id } = useParams();
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState("");
   const [redirect, setRedirect] = useState(false);
 
-  function updatePost(e) {
+  useEffect(() => {
+    fetch(`http://localhost:4000/post/${id}`).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title);
+        setContent(postInfo.content);
+        setSummary(postInfo.summary);
+      });
+    });
+  }, []);
+
+  async function updatePost(e) {
     e.preventDefault();
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("id", id);
+    if (files?.[0]) {
+      data.set("file", files?.[0]);
+    }
+
+    const response = await fetch("http://localhost:4000/post", {
+      method: "PUT",
+      body: data,
+      credentials: "include",
+    });
+
+    if (response.ok) {
+      setRedirect(true);
+    }
   }
 
   if (redirect) {
-    return <Navigate to={"/"} />;
+    return <Navigate to={"/post/" + id} />;
   }
 
   return (
     <form onSubmit={updatePost}>
+      {id}
       <input
         type="title"
         placeholder={"Title"}
@@ -39,7 +67,7 @@ function EditPost() {
       />
       <input type="file" onChange={(e) => setFiles(e.target.files)} />
       <Editor onChange={setContent} value={content} />
-      <button style={{ marginTop: "5px" }}>Create post</button>
+      <button style={{ marginTop: "5px" }}>Update post</button>
     </form>
   );
 }
